fc1d59a704fce5f5f503da8bd540b67c29d39a8e
HoangNT <hoang.nt@anlab.vn>
2014-10-10 13:03:58 +0700

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,31 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+//= require buttons.js
+//= require vmsite
+//= require vmprices
+//= require nx.vm2_ajaxsearch
+//= require mootools-more
+//= require mootools-core
+//= require modal
+//= require jquery.jcarousel
+//= require jquery-ui-1.9.2.min
+//= require jquery-1.8.3.min
+//= require facebox
+//= require dropdown_menu
+//= require mootools-core
+//= require camera.min
+//= require addthis_widget
