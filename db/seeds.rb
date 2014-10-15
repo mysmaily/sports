2bce85bd107e4f9c261dc27c8cf44abb66c7b7a3
HoangNT <hoang.nt@anlab.vn>
2014-10-15 17:29:41 +0700

    add_product_category

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-4.times do 
-  Fabricate(:article, title: Faker::Name.title, description: Faker::Lorem.paragraph,
-            content: Faker::Lorem.paragraph(2), thumn: File.open("#{Rails.root}/app/assets/images/#{rand(1..6)}.jpg"))
-end
+puts "Create User"
+  User.create(:email => 'admin@gmail.com', :password => 'handoichantinh', :password_confirmation => 'handoichantinh')
+  User.create(:email => 'mod@gmail.com', :password => 'handoichantinh', :password_confirmation => 'handoichantinh')
+  User.create(:email => 'user@gmail.com', :password => 'handoichantinh', :password_confirmation => 'handoichantinh')
 
-10.times do
-  category = Fabricate(:category, name: Faker::Name.name)
-    10.times do
-      product = Product.new(name: Faker::Name.name, description: Faker::Lorem.paragraph,
-                thum: File.open("#{Rails.root}/app/assets/images/cake/#{rand(1..6)}.jpg"),
-                price: rand(1000.10000), home: [true,false].sample, price_saleoff: Settings.sale_off)
-      2.times do
-        product.photos << Photo.create!(photo: File.open("#{Rails.root}/app/assets/images/cake/#{rand(7..10)}.jpg") )
+puts "Create Product Category and Product"
+  4.times do 
+    Fabricate(:article, title: Faker::Name.title, description: Faker::Lorem.paragraph,
+              content: Faker::Lorem.paragraph(2), thumn: File.open("#{Rails.root}/app/assets/images/#{rand(1..6)}.jpg"))
+  end
+
+  10.times do
+    category = Fabricate(:category, name: Faker::Name.name)
+      10.times do
+        product = Product.new(name: Faker::Name.name, description: Faker::Lorem.paragraph,
+                  thum: File.open("#{Rails.root}/app/assets/images/cake/#{rand(1..6)}.jpg"),
+                  price: rand(1000.10000), home: [true,false].sample, price_saleoff: Settings.sale_off)
+        2.times do
+          product.photos << Photo.create!(photo: File.open("#{Rails.root}/app/assets/images/cake/#{rand(7..10)}.jpg") )
+        end
+        product.save!
+        category.products << product
       end
-      product.save!
-      category.products << product
-    end
 
-end
+  end
